7e247ffab5c2c3f2e653f6e5e1fc00aa71981161
Vsssel <asselonay@gmail.com>
2024-09-30 11:18:32 +0500

    loader fix

--- src/shared/components/LoaderView.vue
+++ src/shared/components/LoaderView.vue
@@ -1,27 +1,24 @@
 <template>
     <div class="loader card w-100 p-5"></div>
-  </template>
+</template>
   
-  <script setup lang="ts">
-  // No props or logic needed for a basic loader
-  </script>
   
-  <style scoped>
+<style scoped>
   .loader {
     background: linear-gradient(90deg, #e0e0e0 25%, #f0f0f0 50%, #e0e0e0 75%);
-    background-size: 200% 100%; /* For the gradient animation */
-    animation: loading 1.5s infinite; /* Infinite animation */
-    margin: auto; /* Center the loader */
+    background-size: 200% 100%;
+    animation: loading 1.5s infinite;
+    margin: auto;
     border: none;
   }
   
-  @keyframes loading {
+@keyframes loading {
     0% {
-      background-position: 200% 0; /* Start position */
+      background-position: 200% 0;
     }
     100% {
-      background-position: 0 0; /* End position */
+      background-position: 0 0; 
     }
-  }
-  </style>
+}
+</style>
   
